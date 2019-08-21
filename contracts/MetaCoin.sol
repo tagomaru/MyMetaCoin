009fd86cb2557b67c16a1514f10f6e7a729cf28e
tagomaru <tagomaru@users.noreply.github.com>
2019-08-21 23:27:34 +0000

    initial commit

---
+++ contracts/MetaCoin.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.4.25 <0.6.0;
+
+contract MetaCoin {
+	mapping (address => uint) balances;
+
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+	constructor() public {
+		balances[tx.origin] = 10000;
+	}
+
+	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+		if (balances[msg.sender] < amount) return false;
+		balances[msg.sender] -= amount;
+		balances[receiver] += amount;
+		emit Transfer(msg.sender, receiver, amount);
+		return true;
+	}
+
+	function getBalanceInEth(address addr) public view returns(uint){
+		return convert(getBalance(addr),2);
+	}
+
+	function getBalance(address addr) public view returns(uint) {
+		return balances[addr];
+	}
+	
+	function convert(uint amount,uint conversionRate) private pure returns (uint convertedAmount) {
+		return amount * conversionRate;
+	}
+}
